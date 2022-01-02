ade41ed784f0ecf578ef038476f1d07c578d35f7
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-01-02 22:36:33 +0700

    custom colors

---
+++ lib/custom_colors/custom_colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color darkBlue = Color(0xFF2c2c54);
--- lib/font_style/font_style.dart
+++ lib/font_style/font_style.dart
@@ -1,9 +1,10 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter/custom_colors/custom_colors.dart';
 
 TextStyle mainHeader = TextStyle(
     fontSize: 26,
-    color: Colors.red,
+    color: darkBlue,
     fontFamily: 'roboto'
 );
